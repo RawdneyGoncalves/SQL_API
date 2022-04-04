ca69616430406764373c33696122ea1290e8df0f
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-04-04 09:44:20 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const routes = require('./routes')
 const app = express();
-const port = 3333
+const PORT = process.env.PORT || 3333
 require("./database");
 app.use(express.json());
 app.use(routes);
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
